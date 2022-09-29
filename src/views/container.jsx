be82ff206937e226ea46a1f8ee8515357ec4ff83
jcrojas <jcrojas@bancolombia.com.co>
2022-09-28 19:58:55 -0500

    primer commit del front

---
+++ src/views/container.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { NavBar } from './components/navBar/navBar'
+import { Product } from './components/product/product'
+import { ListProduct } from './components/listProduct/listProduct'
+
+function Container() {
+    return (
+        <div>
+            <BrowserRouter>
+                <NavBar />
+                <h1 className="text-center"> UNIVERSAL </h1>
+
+                <Routes>
+                    <Route path="/" element={<Product />} />
+                    <Route path="/crearProducto" element={<Product/>} />
+                    <Route path="/listadoProductos" element={<ListProduct/>} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export { Container }
